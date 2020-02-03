a73d72b4bf7ad7f695b927c9c7bd98be390784e5
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2020-02-03 13:34:24 +0000

    punto 4a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var file_utenti = require('../utenti.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/web', function(req, res, next) {
+  res.render('index', { title: 'Web' });
+});
+
+router.get('/web/:email', function (req, res, next) {
+    let utente_trovato = file_utenti.utenti.find(utente => utente.email == req.params.email);
+    res.render('people', { title: 'Utente', utente: utente_trovato});
 });
 
 module.exports = router;
